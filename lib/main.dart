8effc7b9a6756983cd85942619d8215675fb4cf2
FOODOHOLIC <adarshkohli183@gmail.com>
2023-02-18 20:00:19 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart
    #	lib/otppage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,21 @@
+import 'package:agri_ecommerce/otppage.dart';
+import 'package:agri_ecommerce/sign_up.dart';
 import 'package:flutter/material.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+//import 'otp_page.dart';
+//import 'package:country_picker/country_picker.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//import 'home.dart';
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+        home: MyHomePage(
+      title: 'Sign In',
+    )));
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +24,243 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: const Color(0xff292D32),
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 60),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Welcome 👋",
+                  style: TextStyle(
+                      color: Color(0xffC8CACB),
+                      fontFamily: 'Metropolis',
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Enter Your Mobile Number",
+                  style: TextStyle(
+                      color: Color(0xffC8CACB),
+                      fontFamily: 'Metropolis',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 12),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "You will receive a 4 digit code to verify next. ",
+                  style: TextStyle(
+                      color: Color(0xff4B5158),
+                      fontFamily: 'Metropolis',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 32),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Mobile Number",
+                  style: TextStyle(
+                      color: Color(0xffC8CACB),
+                      fontFamily: 'Metropolis',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12.0, left: 16, right: 16),
+              child: IntlPhoneField(
+                showCountryFlag: true,
+                initialCountryCode: 'INDIA',
+                decoration: const InputDecoration(
+                  counter: Offstage(),
+                  hintText: 'Enter mobile number',
+                  hintStyle:
+                      TextStyle(fontSize: 15.0, color: Color(0xffC8CACB)),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                  ),
+                ),
+                onChanged: (phone) {
+                  print(phone.completeNumber);
+                },
+                onCountryChanged: (country) {
+                  print('Country changed to: ' + country.name);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 16, right: 16),
+              child: Container(
+                height: 50,
+                width: 380,
+                decoration: BoxDecoration(
+                    color: const Color(0xffA9DFD8),
+                    borderRadius: BorderRadius.circular(25)),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => OtpPage()));
+                  },
+                  child: const Text(
+                    'Send OTP',
+                    style: TextStyle(
+                        color: Color(0xff000000),
+                        fontFamily: 'Metropolis',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'or Continue with',
+              style: TextStyle(
+                  color: Color(0xffC8CACB),
+                  fontFamily: 'Metropolis',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    height: 54,
+                    width: 164,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xff8f9586),
+                        ),
+                        color: const Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => SignUpPage()));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: SizedBox(
+                                  height: 20,
+                                  width: 20,
+                                  child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                          color: const Color(0xffffffff),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Image.asset(
+                                        'assets/google_icon.png',
+                                        // color: Color(0xff603183),
+                                      ))),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 11.0),
+                              child: Text(
+                                'Google',
+                                style: TextStyle(
+                                    color: Color(0xffeb4132),
+                                    fontFamily: 'Metropolis',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    height: 54,
+                    width: 164,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xff8f9586),
+                        ),
+                        color: const Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => MyHomePage(
+                                      title: 'gtr',
+                                    )));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: SizedBox(
+                                  height: 20,
+                                  width: 20,
+                                  child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                          color: const Color(0xffffffff),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Image.asset(
+                                        'assets/apple_icon.png',
+                                        // color: Color(0xff603183),
+                                      ))),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 11.0),
+                              child: Text(
+                                'Apple',
+                                style: TextStyle(
+                                    color: Color(0xff141a39),
+                                    fontFamily: 'Metropolis',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
