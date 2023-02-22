970a6ec97179259ed3519912e6bc7d52a35e8391
ANUKULJHA1506 <studious.anukul@gmail.com>
2023-02-22 14:22:21 +0530

    Merge remote-tracking branch 'github/master'

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,8 +6,6 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:flutter/rendering.dart';
 import 'package:line_icons/line_icons.dart';
 
-
-
 // ignore: camel_case_types
 class home_page extends StatefulWidget {
   const home_page({Key? key}) : super(key: key);
@@ -15,12 +13,8 @@ class home_page extends StatefulWidget {
   State<home_page> createState() => _home_pageState();
 }
 
-
-
-
 class _home_pageState extends State<home_page> {
-
-   late PageController pageController;
+  late PageController pageController;
   final ScrollController _scrollController = ScrollController();
   int pageNo = 0;
 
@@ -57,189 +51,188 @@ class _home_pageState extends State<home_page> {
     super.initState();
   }
 
-
-
-
   @override
   void dispose() {
     pageController.dispose();
     super.dispose();
   }
+
   bool showBtmAppBr = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff292D32),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Color(0xff292D32),
-            expandedHeight: 110,
-            floating: true,
-            pinned: true,
-            snap: false,
-            centerTitle: false,
-            automaticallyImplyLeading: false,
-
-            title: const Text('Agri_Tech'),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.shopping_cart),
-                onPressed: () {},
-              ),
-            ],
-            bottom: AppBar(backgroundColor: Color(0xff292D32),
-              title: Container(
-                width: double.infinity,
-                height: 40,
-                color: Colors.white,
-                child: const Center(
-                  child: TextField(
-                    decoration: InputDecoration( iconColor:Color(0xff292D32) ,
-                        hintText: 'Search for something',
-                        prefixIcon: Icon(Icons.search , color: Color(0xff292D32),),
-                        suffixIcon: Icon(Icons.camera_alt,color: Color(0xff292D32))),
-                  ),
+        backgroundColor: Color(0xff292D32),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Color(0xff292D32),
+              expandedHeight: 110,
+              floating: true,
+              pinned: true,
+              snap: false,
+              centerTitle: false,
+              automaticallyImplyLeading: false,
+              title: const Text('Agri_Tech'),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.shopping_cart),
+                  onPressed: () {},
                 ),
-              ),
-            ),
-          ),
-
-          // Other Sliver Widgets
-          SliverList(
-            delegate: SliverChildListDelegate([
-              SizedBox(
-                height: 210,
-                width: 300,
-                child: PageView.builder(
-                  controller: pageController,
-                  onPageChanged: (index) {
-                    pageNo = index;
-                    setState(() {});
-                  },
-                  itemBuilder: (_, index) {
-                    return AnimatedBuilder(
-                      animation: pageController,
-                      builder: (ctx, child) {
-                        return child!;
-                      },
-                      child: GestureDetector(
-                        onTap: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content:
-                              Text("Hello you tapped at ${index + 1} "),
-                            ),
-                          );
-                        },
-                        onPanDown: (d) {
-                          carasouelTmer?.cancel();
-                          carasouelTmer = null;
-                        },
-                        onPanCancel: () {
-                          carasouelTmer = getTimer();
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(
-                              right: 8, left: 8, top: 5, bottom: 12),
-                          decoration: BoxDecoration(
-                            color: Color(0xffA9DFD8),
-                            image: const DecorationImage(
-                                image: AssetImage("assets/mask_group.png"),
-                                fit: BoxFit.cover),
-
-                            borderRadius: BorderRadius.circular(25.0),
-                            //color: const Color(0xffFF6933),
+              ],
+              bottom: AppBar(
+                backgroundColor: Color(0xff292D32),
+                title: Container(
+                  width: double.infinity,
+                  height: 40,
+                  color: Colors.white,
+                  child: const Center(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          iconColor: Color(0xff292D32),
+                          hintText: 'Search for something',
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color(0xff292D32),
                           ),
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: 3,
-                ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  3,
-                      (index) => GestureDetector(
-                    child: Container(
-                      margin: const EdgeInsets.only(
-                        top: 2.0,
-                        left: 5,
-                      ),
-                      child: Icon(
-                        Icons.circle,
-                        size: 10.0,
-                        color: pageNo == index
-                            ? const Color(0xffA9DFD8)
-                            : const Color(0xff8f9586),
-                      ),
+                          suffixIcon:
+                              Icon(Icons.camera_alt, color: Color(0xff292D32))),
                     ),
                   ),
                 ),
               ),
-              Container(
-                height: 1000,
-                color: Color(0xff292D32),
+            ),
 
+            // Other Sliver Widgets
+            SliverList(
+              delegate: SliverChildListDelegate([
+                SizedBox(
+                  height: 210,
+                  width: 300,
+                  child: PageView.builder(
+                    controller: pageController,
+                    onPageChanged: (index) {
+                      pageNo = index;
+                      setState(() {});
+                    },
+                    itemBuilder: (_, index) {
+                      return AnimatedBuilder(
+                        animation: pageController,
+                        builder: (ctx, child) {
+                          return child!;
+                        },
+                        child: GestureDetector(
+                          onTap: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                    Text("Hello you tapped at ${index + 1} "),
+                              ),
+                            );
+                          },
+                          onPanDown: (d) {
+                            carasouelTmer?.cancel();
+                            carasouelTmer = null;
+                          },
+                          onPanCancel: () {
+                            carasouelTmer = getTimer();
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                                right: 8, left: 8, top: 5, bottom: 12),
+                            decoration: BoxDecoration(
+                              color: Color(0xffA9DFD8),
+                              image: const DecorationImage(
+                                  image: AssetImage("assets/mask_group.png"),
+                                  fit: BoxFit.cover),
+
+                              borderRadius: BorderRadius.circular(25.0),
+                              //color: const Color(0xffFF6933),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: 3,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    3,
+                    (index) => GestureDetector(
+                      child: Container(
+                        margin: const EdgeInsets.only(
+                          top: 2.0,
+                          left: 5,
+                        ),
+                        child: Icon(
+                          Icons.circle,
+                          size: 10.0,
+                          color: pageNo == index
+                              ? const Color(0xffA9DFD8)
+                              : const Color(0xff8f9586),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 1000,
+                  color: Color(0xff292D32),
+                ),
+              ]),
+            ),
+          ],
+        ),
+        bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              color: Color(0xff292D32),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 20,
+                  color: Colors.black.withOpacity(.1),
+                )
+              ],
+            ),
+            child: SafeArea(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+                child: GNav(
+                  rippleColor: Colors.grey[300]!,
+                  hoverColor: Colors.grey[300]!,
+                  gap: 8,
+                  activeColor: Colors.black,
+                  iconSize: 24,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  duration: Duration(milliseconds: 400),
+                  tabBackgroundColor: Color(0xffA9DFD8),
+                  color: Colors.grey[100]!,
+                  tabs: const [
+                    GButton(
+                      icon: LineIcons.home,
+                      text: 'Home',
+                    ),
+                    GButton(
+                      icon: LineIcons.amazonPay,
+                      text: 'Payment',
+                    ),
+                    GButton(
+                      icon: LineIcons.percentage,
+                      text: 'Cashback',
+                    ),
+                    GButton(
+                      icon: LineIcons.user,
+                      text: 'Profile',
+                    ),
+                  ],
+                ),
               ),
-            ]),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-      decoration: BoxDecoration(
-      color: Color(0xff292D32),
-      boxShadow: [
-        BoxShadow(
-          blurRadius: 20,
-          color: Colors.black.withOpacity(.1),
-        )
-      ],
-    ),
-    child: SafeArea(
-    child: Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-    child: GNav(
-    rippleColor: Colors.grey[300]!,
-    hoverColor: Colors.grey[300]!,
-    gap: 8,
-    activeColor: Colors.black,
-    iconSize: 24,
-    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-    duration: Duration(milliseconds: 400),
-    tabBackgroundColor: Color(0xffA9DFD8),
-    color: Colors.grey[100]!,
-    tabs: const [
-    GButton(
-    icon: LineIcons.home,
-    text: 'Home',
-    ),
-    GButton(
-    icon: LineIcons.amazonPay,
-    text: 'Payment',
-    ),
-    GButton(
-    icon: LineIcons.percentage,
-    text: 'Cashback',
-    ),
-    GButton(
-    icon: LineIcons.user,
-    text: 'Profile',
-    ),
-    ],
-
-    ),
-    ),))
-    );
+            )));
   }
 }
-
-
-
-
